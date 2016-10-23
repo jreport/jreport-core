d6b474bcc2e70e0c2051572735da480530101489
jcruz <jcruz@rpawsds01.redetre.jus.br>
2016-10-23 15:20:54 -0300

    ajuste no newLine

--- jreport-core/src/main/java/br/jreport/core/api/NewLine.java
+++ jreport-core/src/main/java/br/jreport/core/api/NewLine.java
@@ -1,8 +1,7 @@
 package br.jreport.core.api;
 
 import java.io.Serializable;
-import java.util.Optional;
 
 public interface NewLine<T> extends Serializable {
-	Optional<T> build();
+	void build();
 }
